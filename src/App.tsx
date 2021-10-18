def3414f28ebcf50248599a683626a989697596e
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-10-18 16:48:08 +0200

    Add ui layer for handling store

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux"
+import { Dispatchers } from "./store"
+import { RootState } from "./store/Reducers"
 import './App.css';
 
 function App() {
+  const state = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+  const { AddNumber, ReduceNumber, DefaultNumber } = bindActionCreators(Dispatchers, dispatch);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{state.counter}</h1>
+        <button onClick={() => AddNumber(1)}>Add number</button>
+        <button onClick={() => ReduceNumber(1)}>Reduce number</button>
+        <button onClick={() => DefaultNumber()}>Reset</button>
       </header>
     </div>
   );
